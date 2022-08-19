d127066f0368f735704d4a151197764186950b3b
Clas <raphaelcalixto2013@gmail.com>
2022-08-18 22:35:46 -0400

    Gestão de funcionário 0.0

---
+++ Exercícios/Python/GestãoDeFuncionario/GDF.py
@@ -0,0 +1,29 @@
+# -- Cadastro de informações do Usuario --#
+
+# Main Imports...
+
+# POO Imports...
+
+
+# -- Classe | main -- ->>
+class GDF(object):
+    """docstring for GDF."""
+
+    def __init__(self, arg):
+        super(GDF, self).__init__()
+        self.arg = arg
+
+    def run_Events(self):
+        pass
+
+    def user_INFO(self):
+        pass
+
+    def end_program(self):
+        pass
+
+
+# -- Classe | Chamada -- ->> <<-
+if __main__ == '__main__':
+    CadastroDeFuncionario = GDF()
+    CadastroDeFuncionario.run_Events()
