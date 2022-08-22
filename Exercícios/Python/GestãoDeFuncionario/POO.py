e2d992022357ed771b0ab4c166576c7822dac5b0
Clas <raphaelcalixto2013@gmail.com>
2022-08-21 21:11:24 -0400

    Refaturamento de tratamentos

--- Exercícios/Python/GestãoDeFuncionario/POO.py
+++ Exercícios/Python/GestãoDeFuncionario/POO.py
@@ -1,20 +1,24 @@
 # -- POO | Otimizações --
 
 # -- Função | -- <<- Desconto ->> --
-def desconto(d, s): return (d * s) / 100
+def desconto(d: int, s: int): return (d * s) / 100
+
+# -- Função | -- <<- Tratamento de string ->> --
+def tratamento(value, ms_error, obj):
+    while 1:
+        input_ = input(value).strip().capitalize()
+        if obj['teste'](input_): break
+        else: print(ms_error)
+    return obj['tipo'](input_)
 
 # -- Função | -- <<- Tratamento de string ->> --
 def string(s, ms_error):
-    while 1:
-        input_str: str = input(s).strip().capitalize()
-        if input_str.isalpha(): break
-        else: print(ms_error)
-    return str(input_str)
+    return tratamento(s, ms_error,
+    {'tipo': lambda v : str(v),
+     'teste':  lambda v : v.isalpha()})
 
 # -- Função | -- <<- Tratamento de int | float ->> --
 def inteiro(i, ms_error):
-    while 1:
-        input_int: int = input(i).strip().capitalize()
-        if input_int.isnumeric(): break
-        else: print(ms_error)
-    return int(input_int)
+    return tratamento(s, ms_error,
+    {'tipo': lambda v : int(v),
+     'teste':  lambda v : v.isnumeric()})
